dbf61528cfa37c5180a02801f0d44b7d47ceee90
junaid ali <alik2624@gmail.com>
2024-10-23 03:54:16 -0700

    Old next config but output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,92 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "nodejs.org",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "seeklogo.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "laravel.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.iconscout.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.vectorlogo.zone",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "static.xx.fbcdn.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn4.iconfinder.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn3.iconfinder.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "pixelpk.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "icon.icepanel.io",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "d2jdgazzki9vjm.cloudfront.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "brandslogos.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.freepik.com",
-        pathname: "/**",
-      },
+    domains: [
+      "nodejs.org",
+      "seeklogo.com",
+      "laravel.com",
+      "cdn.iconscout.com",
+      "www.vectorlogo.zone",
+      "static.xx.fbcdn.net",
+      "cdn4.iconfinder.com",
+      "cdn3.iconfinder.com",
+      "storage.googleapis.com",
+      "pixelpk.com",
+      "icon.icepanel.io",
+      "amazonaws.com",
+      "github.com",
+      "d2jdgazzki9vjm.cloudfront.net",
+      "brandslogos.com",
+      "img.freepik.com",
     ],
-    // Add this for local images
-    unoptimized: false, // Keep image optimization
-    dangerouslyAllowSVG: true, // If you're using SVG images
   },
+  output: "export",
 };
+
 
 export default nextConfig;
